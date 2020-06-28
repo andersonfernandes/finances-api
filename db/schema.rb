b2ea95000b30a0da9cfc4c48ffd42b9291fd68a3
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-06-28 12:20:20 -0300

    feature/25 Update db config
    - Add env vars to database.yml
    - Removed unused code from schema
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -14,26 +14,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_022603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "budget_items", force: :cascade do |t|
-    t.string "description", null: false
-    t.decimal "amount", null: false
-    t.decimal "min_amount", null: false
-    t.bigint "budget_id"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["budget_id"], name: "index_budget_items_on_budget_id"
-    t.index ["category_id"], name: "index_budget_items_on_category_id"
-  end
-
-  create_table "budgets", force: :cascade do |t|
-    t.string "description", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_budgets_on_user_id"
-  end
 
   create_table "categories", force: :cascade do |t|
     t.string "description", null: false
@@ -64,9 +44,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_022603) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "budget_items", "budgets"
-  add_foreign_key "budget_items", "categories"
-  add_foreign_key "budgets", "users"
   add_foreign_key "categories", "users"
   add_foreign_key "expenses", "categories"
   add_foreign_key "expenses", "users"
