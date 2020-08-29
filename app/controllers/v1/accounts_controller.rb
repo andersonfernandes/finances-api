b3b02c12353202f2ec6a93e4d9cd90e6935c6566
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-08-29 00:24:03 -0300

    feature/29 Add accounts index doc
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/controllers/v1/accounts_controller.rb
+++ app/controllers/v1/accounts_controller.rb
@@ -1,5 +1,25 @@
 module V1
   class AccountsController < ApplicationController
+    resource_description do
+      short 'Accounts Actions'
+      error code: 401, desc: 'Unauthorized'
+      error code: 400, desc: 'Bad Request'
+      error code: 404, desc: 'Not Found'
+      error code: 422, desc: 'Unprocessable Entity'
+      formats ['json']
+    end
+
+    def_param_group :account do
+      property :id, :number, desc: 'Account id'
+      property :description, String, desc: 'Account description'
+      property :financial_institution,
+               String, desc: 'Account related financial_institution'
+      property :initial_amount, :decimal, desc: 'Account initial amount'
+      property :account_type, Account.account_types.keys
+    end
+
+    api :GET, '/v1/accounts', 'List all user accounts'
+    returns array_of: :account, code: 200, desc: 'Successful response'
     def index
       accounts = Account.where(user_id: current_user.id)
 
