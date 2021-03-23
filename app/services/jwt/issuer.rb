997b8886598e52707f182f6c199c90252a1a086d
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-03-23 00:45:02 -0300

    Updated jwt issuer
    - added access token encoding impl
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/services/jwt/issuer.rb
+++ app/services/jwt/issuer.rb
@@ -6,11 +6,23 @@ module Jwt
 
     def call
       {
+        access_token: access_token,
         refresh_token: refresh_token
       }
     end
 
     private
+
+    ACCESS_TOKEN_EXPIRY = 2.hours.from_now.to_i
+
+    def access_token
+      payload = {
+        exp: ACCESS_TOKEN_EXPIRY,
+        user_id: @user.id
+      }
+
+      JWT.encode(payload, Figaro.env.secret_key_base)
+    end
 
     def refresh_token
       return @user.refresh_token.token if @user.refresh_token
@@ -24,5 +36,5 @@ module Jwt
         status: :active
       )
     end
-  end 
+  end
 end
--- spec/services/jwt/issuer_spec.rb
+++ spec/services/jwt/issuer_spec.rb
@@ -5,32 +5,35 @@ describe Jwt::Issuer do
   subject { described_class.new(user) }
 
   describe '#call' do
+    it 'should include the encoded access_token' do
+      response = subject.call
+      expect(response[:access_token]).not_to be_blank
+    end
+
     context 'when the user have a active refresh_token associated' do
       let!(:refresh_token) { create(:refresh_token, status: :active, user: user) }
 
       it 'should not create a new refresh token' do
-        expect {
-          subject.call
-        }.not_to change { RefreshToken.count }
+        expect { subject.call }
+          .not_to(change { RefreshToken.count })
       end
 
       it 'should include the existing token on the response' do
-        expect(subject.call).to eq({ refresh_token: refresh_token.token })
+        expect(subject.call).to include(refresh_token: refresh_token.token)
       end
     end
 
     context 'when the user does not have an active refresh_token associated' do
       it 'should create a new refresh token' do
-        expect {
-          subject.call
-        }.to change { RefreshToken.count }.by(1)
+        expect { subject.call }
+          .to change { RefreshToken.count }.by(1)
       end
 
       it 'should include the existing token on the response' do
         response = subject.call
         created_refresh_token = RefreshToken.last
 
-        expect(response).to eq({ refresh_token: created_refresh_token.token }) 
+        expect(response).to include(refresh_token: created_refresh_token.token)
       end
     end
   end
