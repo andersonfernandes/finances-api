41b9883b9ff4778f7151d77c4652278e81e02b5e
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-04-01 00:13:33 -0300

    Removed refresh token destroy on revoke
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/services/jwt/revoker.rb
+++ app/services/jwt/revoker.rb
@@ -1,9 +1,8 @@
 module Jwt
   class Revoker
     def call(access_token)
-      user, token = Jwt::Authenticator.new.call(access_token)
+      _, token = Jwt::Authenticator.new.call(access_token)
 
-      user.refresh_token.destroy
       token.revoked!
     rescue Jwt::Errors::RevokedToken
       true
--- spec/services/jwt/revoker_spec.rb
+++ spec/services/jwt/revoker_spec.rb
@@ -7,19 +7,20 @@ describe Jwt::Revoker do
       create(:refresh_token, user: user)
       user
     end
+    let(:token) { create(:token, user: user, status: token_status) }
+    let(:access_token) do
+      JWT.encode(
+        {
+          user_id: user.id,
+          exp: 2.days.from_now.to_i,
+          jti: token.jwt_id
+        },
+        Figaro.env.secret_key_base
+      )
+    end
 
     context 'with a valid access_token' do
-      let(:token) { create(:token, user: user, status: :active) }
-      let(:access_token) do
-        JWT.encode(
-          {
-            user_id: user.id,
-            exp: 2.days.from_now.to_i,
-            jti: token.jwt_id
-          },
-          Figaro.env.secret_key_base
-        )
-      end
+      let(:token_status) { :active }
 
       it { expect(subject.call(access_token)).to eq(true) }
 
@@ -27,25 +28,10 @@ describe Jwt::Revoker do
         subject.call(access_token)
         expect(token.reload.status).to eq('revoked')
       end
-
-      it 'the user refresh token should be destroyed' do
-        subject.call(access_token)
-        expect(user.reload.refresh_token).to be_nil
-      end
     end
 
     context 'with an already revoked token' do
-      let(:token) { create(:token, user: user, status: :revoked) }
-      let(:access_token) do
-        JWT.encode(
-          {
-            user_id: user.id,
-            exp: 2.days.from_now.to_i,
-            jti: token.jwt_id
-          },
-          Figaro.env.secret_key_base
-        )
-      end
+      let(:token_status) { :revoked }
 
       it { expect(subject.call(access_token)).to eq(true) }
 
