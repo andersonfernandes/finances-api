a56c33738846c5efcd1181d24a007dc335821eab
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-03-31 23:22:51 -0300

    Refactor decoder spec
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- spec/services/jwt/decoder_spec.rb
+++ spec/services/jwt/decoder_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 describe Jwt::Decoder do
   describe '#call' do
+    let(:user) { create(:user) }
+    let(:access_token) do
+      JWT.encode(
+        {
+          user_id: user.id,
+          exp: token_expiry_at
+        },
+        Figaro.env.secret_key_base
+      )
+    end
+
     context 'with an valid access_token' do
-      let(:user) { create(:user) }
-      let(:access_token) do
-        JWT.encode(
-          {
-            user_id: user.id,
-            exp: 2.days.from_now.to_i
-          },
-          Figaro.env.secret_key_base
-        )
-      end
+      let(:token_expiry_at) { 2.days.from_now.to_i }
 
       it do
         expected_result = {
@@ -25,23 +27,15 @@ describe Jwt::Decoder do
     end
 
     context 'with an invalid access_token' do
+      let(:access_token) { 'invalid_access_token' }
+
       it do
-        expect { subject.call('invalid_token') }
-          .to raise_error(Jwt::Errors::InvalidToken)
+        expect { subject.call(access_token) }.to raise_error(Jwt::Errors::InvalidToken)
       end
     end
 
     context 'with an expired access_token' do
-      let(:user) { create(:user) }
-      let(:access_token) do
-        JWT.encode(
-          {
-            user_id: user.id,
-            exp: 2.days.ago.to_i
-          },
-          Figaro.env.secret_key_base
-        )
-      end
+      let(:token_expiry_at) { 2.days.ago.to_i }
 
       it do
         expect { subject.call(access_token) }.to raise_error(Jwt::Errors::ExpiredToken)
