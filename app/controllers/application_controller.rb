0a01a5fc732bde7f22bfd8d6b57c67a826256ca2
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-04-05 01:14:43 -0300

    Removed header_signature_error method
    - added metadata to the jwt errors
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,21 @@ class ApplicationController < ActionController::API
   rescue_from Jwt::Errors::InvalidToken,
               Jwt::Errors::MissingToken,
               Jwt::Errors::ExpiredToken,
-              Jwt::Errors::RevokedToken do |e|
-    response.headers['WWW-Authenticate'] = build_authenticate_header(e.class.to_s)
-    render error_response(:unauthorized, e.message)
+              Jwt::Errors::RevokedToken do |error|
+    response.headers['WWW-Authenticate'] = build_authenticate_header(error)
+    render error_response(:unauthorized, error.message)
   end
 
-  rescue_from ActiveRecord::RecordNotFound do |e|
-    render error_response(:not_found, e.message)
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render error_response(:not_found, error.message)
   end
 
-  rescue_from Apipie::ParamMissing, Jwt::Errors::MissingRefreshToken do |e|
-    render error_response(:bad_request, e.message)
+  rescue_from Apipie::ParamMissing, Jwt::Errors::MissingRefreshToken do |error|
+    render error_response(:bad_request, error.message)
   end
 
-  rescue_from Apipie::ParamInvalid do |e|
-    render error_response(:unprocessable_entity, e.message)
+  rescue_from Apipie::ParamInvalid do |error|
+    render error_response(:unprocessable_entity, error.message)
   end
 
   def error_response(http_error, errors = {})
@@ -39,29 +39,15 @@ class ApplicationController < ActionController::API
     request.headers.fetch('Authorization', '').split(' ').last
   end
 
-  def build_authenticate_header(error_class)
+  def build_authenticate_header(error)
+    error_identifier = error&.error_identifier || 'invalid_token'
     header_signature = [
       "Bearer realm=\"#{request.base_url}\"",
-      "error=\"#{header_signature_error(error_class)[:key]}\"",
-      "error_description=\"#{header_signature_error(error_class)[:description]}\""
+      "error=\"#{error_identifier}\"",
+      "error_description=\"#{error.message}\""
     ]
 
     header_signature.join(', ')
-  end
-
-  def header_signature_error(error_class)
-    invalid_token_error = { key: 'invalid_token', description: 'Invalid access token' }
-    expired_token_error = { key: 'expired_token', description: 'Access token expired' }
-    revoked_token_error = { key: 'revoked_token', description: 'Access token revoked' }
-
-    signature_error_by_class = {
-      'Jwt::Errors::MissingToken' => invalid_token_error,
-      'Jwt::Errors::InvalidToken' => invalid_token_error,
-      'Jwt::Errors::ExpiredToken' => expired_token_error,
-      'Jwt::Errors::RevokedToken' => revoked_token_error
-    }
-
-    signature_error_by_class[error_class]
   end
 
   def error_template(http_error, errors)
--- app/services/jwt/errors.rb
+++ app/services/jwt/errors.rb
@@ -4,11 +4,19 @@ module Jwt
       def message
         'Invalid Access Token'
       end
+
+      def error_identifier
+        'invalid_token'
+      end
     end
 
     class ExpiredToken < StandardError
       def message
         'Expired Access Token'
+      end
+
+      def error_identifier
+        'expired_token'
       end
     end
 
@@ -16,11 +24,19 @@ module Jwt
       def message
         'Revoked Access Token'
       end
+
+      def error_identifier
+        'revoked_token'
+      end
     end
 
     class MissingToken < StandardError
       def message
         'Missing Access Token'
+      end
+
+      def error_identifier
+        'invalid_token'
       end
     end
 
